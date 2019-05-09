6cbfca08af51ffabd1568584e6423179ee172ceb
mahogany eggplant <ym081014@gmail.com>
2019-05-09 22:08:33 +0900

    GoForwardUnityChan
    課題

---
+++ Assets/CubeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	public AudioClip cubeSE;
+	AudioSource aud;
+
+	// キューブの移動速度
+	private float speed = -0.2f;
+
+	// 消滅位置
+	private float deadLine = -12;
+
+	// Use this for initialization
+	void Start () {
+		this.aud = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// キューブを移動させる
+		transform.Translate (this.speed, 0, 0);
+
+		// 画面外に出たら破棄する
+		if (transform.position.x < this.deadLine) {
+			Destroy (gameObject);
+		}
+	}
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.tag == "SETag") {
+			this.aud.PlayOneShot (this.cubeSE);
+		}
+	}
+}
